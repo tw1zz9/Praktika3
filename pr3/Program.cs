b05abda96d5dbe85e520b92e06cf99674996c444
Kirill <volkov_k06@mail.ru>
2025-10-08 11:45:05 +0700

    sirost

--- pr3/Program.cs
+++ pr3/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace pr3
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace pr3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ArrayList uncommunicatedСollection = new ArrayList();
+
+            Random rndm = new Random();
+
+            for (int i = 0; i < 5; i++)
+                uncommunicatedСollection.Add(rndm.Next(1, 100));
+
+            string str = "DobavlennayaStroka";
+
+            uncommunicatedСollection.Add(str);
+
+            for (int i = 0; i < uncommunicatedСollection.Count; i++)
+                Console.Write(uncommunicatedСollection[i] + " ");
+
+            int del_ind = 0;
+            Console.WriteLine("\nВыберите номер элемента для удаления: ");
+            del_ind = int.Parse(Console.ReadLine()) - 1;
+
+            uncommunicatedСollection.Remove(uncommunicatedСollection[del_ind]);
+            Console.WriteLine("Кол-во элементов: " + uncommunicatedСollection.Count);
+            Console.WriteLine("Коллекция:");
+
+            for (int i = 0; i < uncommunicatedСollection.Count; i++)
+                Console.Write(uncommunicatedСollection[i] + " ");
         }
     }
 }
