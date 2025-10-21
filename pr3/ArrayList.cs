693add46c49d84ee9d15d9d00b2e6cd3ad628aa4
host1l3 <timvitkulevskij@gmail.com>
2025-10-21 12:11:32 +0700

    Переезд первой части работы
    Я перенёс первую часть работы (создание ArrayList и работа с ним) в отдельный файл для более простого обращения если будет нужно в любом случае

---
+++ pr3/ArrayList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr3
+{
+    public class ArList
+    {
+        public void WorkWithArrayLIst()
+        {
+            ArrayList uncommunicatedСollection = new ArrayList();
+
+            Random rndm = new Random();
+
+            for (int i = 0; i < 5; i++)
+                uncommunicatedСollection.Add(rndm.Next(1, 100));
+
+            string str = "DobavlennayaStroka";
+
+            uncommunicatedСollection.Add(str);
+
+            for (int i = 0; i < uncommunicatedСollection.Count; i++)
+                Console.Write(uncommunicatedСollection[i] + " ");
+
+            int del_ind = 0;
+            Console.WriteLine("\nВыберите номер элемента для удаления: ");
+            del_ind = int.Parse(Console.ReadLine()) - 1;
+
+            uncommunicatedСollection.Remove(uncommunicatedСollection[del_ind]);
+            Console.WriteLine("Кол-во элементов: " + uncommunicatedСollection.Count);
+            Console.WriteLine("Коллекция:");
+
+            for (int i = 0; i < uncommunicatedСollection.Count; i++)
+                Console.Write(uncommunicatedСollection[i] + " ");
+        }
+    }
+}
--- pr3/Program.cs
+++ pr3/Program.cs
@@ -8,30 +8,8 @@ namespace pr3
     {
         static void Main(string[] args)
         {
-            ArrayList uncommunicatedСollection = new ArrayList();
-
-            Random rndm = new Random();
-
-            for (int i = 0; i < 5; i++)
-                uncommunicatedСollection.Add(rndm.Next(1, 100));
-
-            string str = "DobavlennayaStroka";
-
-            uncommunicatedСollection.Add(str);
-
-            for (int i = 0; i < uncommunicatedСollection.Count; i++)
-                Console.Write(uncommunicatedСollection[i] + " ");
-
-            int del_ind = 0;
-            Console.WriteLine("\nВыберите номер элемента для удаления: ");
-            del_ind = int.Parse(Console.ReadLine()) - 1;
-
-            uncommunicatedСollection.Remove(uncommunicatedСollection[del_ind]);
-            Console.WriteLine("Кол-во элементов: " + uncommunicatedСollection.Count);
-            Console.WriteLine("Коллекция:");
-
-            for (int i = 0; i < uncommunicatedСollection.Count; i++)
-                Console.Write(uncommunicatedСollection[i] + " ");
+            ArList arrList = new ArList();
+            arrList.WorkWithArrayLIst();
         }
     }
 }
